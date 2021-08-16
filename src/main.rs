282fcc017cd8ef4c219f83c57512cbc94d5944b4
Matthew Pagan <mastapegs01@gmail.com>
2021-08-15 23:08:37 -0400

    implement iteration through array

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,24 @@ mod utils;
 use utils::handle_job_title;
 
 fn main() {
-    let me: Person = Person {
-        name: "Matthew Pagan".to_string(),
-        age: 32,
-        hobby: "Coding".to_string(),
-        job_title: Some("Software Developer".to_string()),
-        favorite_color: Color::Cyan,
-    };
+    let people: [Person; 2] = [
+        Person {
+            name: "Matthew Pagan".to_string(),
+            age: 32,
+            hobby: "Coding".to_string(),
+            job_title: Some("Software Developer".to_string()),
+            favorite_color: Color::Cyan,
+        },
+        Person {
+            name: "Noah Pagan".to_string(),
+            age: 5,
+            hobby: "Lego".to_string(),
+            job_title: None,
+            favorite_color: Color::Blue,
+        },
+    ];
 
-    let noah: Person = Person {
-        name: "Noah Pagan".to_string(),
-        age: 5,
-        hobby: "Lego".to_string(),
-        job_title: None,
-        favorite_color: Color::Blue,
-    };
-
-    handle_job_title(me);
-    handle_job_title(noah);
+    for person in people.iter() {
+        handle_job_title(person);
+    }
 }
--- src/utils/handle_job_title.rs
+++ src/utils/handle_job_title.rs
@@ -1,7 +1,7 @@
 use super::super::types::Person;
 
-pub fn handle_job_title(person: Person) -> () {
-    match person.job_title {
+pub fn handle_job_title(person: &Person) -> () {
+    match &person.job_title {
         Some(title) => println!("{} is a {}", person.name, title),
         None => println!("{} doesn't have a job", person.name),
     }
