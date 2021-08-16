fe59d1dea813487973752352898d82eea40d9eca
Matthew Pagan <mastapegs01@gmail.com>
2021-08-15 21:15:01 -0400

    working import of module

---
+++ src/custom_types/person.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub struct Person {
+    pub name: String,
+    pub age: i32,
+    pub hobby: String,
+    pub job_title: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod custom_types;
+
+use custom_types::Person;
+
 fn main() {
-    println!("Hello, world!");
+    let _an_int: i32 = 32;
+    let _a_string: &str = "Hello, world!";
+    let _a_bool: bool = true;
+    let _an_array: [i32; 3] = [1, 2, 3];
+
+    let me: Person = Person {
+        name: "Matthew Pagan".to_string(),
+        age: 32,
+        hobby: "Coding".to_string(),
+        job_title: "Software Developer".to_string(),
+    };
+
+    println!("{:?}", me);
 }
